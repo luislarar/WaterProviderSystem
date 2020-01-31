2ae499b1c2a5b314919d78d943501a121e73a3e7
Luis Lara <rluislara12@hotmail.com>
2020-01-30 22:27:48 -0500

    Agregada app, actualizados settings, y agregada bd para heroku

---
+++ waterprovidersystem/waterprovidersystem/database.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd229rc6s1h64qv',
+        'USER': 'odjgzdrqdakfew',
+        'PASSWORD': '04fbcc54675a3846bb41fe35753c7d49c1cf0fed97004d8907206efc36da892d',
+        'HOST': 'ec2-52-45-75-24.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'connect_timeout': None,
+        }
+    },
+}
+
--- waterprovidersystem/waterprovidersystem/settings.py
+++ waterprovidersystem/waterprovidersystem/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -72,14 +74,7 @@ WSGI_APPLICATION = 'waterprovidersystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+from .database import DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
