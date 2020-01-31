2f924aa9bc21bd75c96a90ed15b06ea21ea9462e
Luis Lara <rluislara12@hotmail.com>
2020-01-31 15:47:36 -0500

    Renamed app

--- waterprovidersystem/waterprovidersystem/urls.py
+++ waterprovidersystem/waterprovidersystem/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-import core.urls as urls_core
+import coreapp.urls as urls_core
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
